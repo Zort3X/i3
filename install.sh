40a9a5f581b6b7698aec989f1bbb0a60702dae5a
Z3R7 <marty.kamenow@gmail.com@>
2024-12-24 13:38:41 +0200

    changes to isntall script

--- install.sh
+++ install.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
 PACMAN_PACKAGES=(
-  "alacritty" "base-devel" "btop" "file-roller" "hyprland" "hyprpaper" "ly" "nano" "neofetch" "nvidia" "pulsemixer" "steam" "thunar" "waybar" "rofi-wayland" "plymouth"
+  "alacritty" "base-devel" "btop" "file-roller" "nano" "neofetch" "pulsemixer" "steam" "thunar" "polybar" "rofi" "plymouth"
 )
 
 PARU_PACKAGES=(
-  "bibata-cursor-theme-bin" "google-chrome" "gruvbox-gtk-theme-git" "gruvbox-icon-theme-git" "hyprshot" "spicetify-cli" "spotify" "vesktop-bin" "plymouth-theme-loader-2-git"
+  "bibata-cursor-theme-bin" "google-chrome" "gruvbox-gtk-theme-git" "gruvbox-icon-theme-git" "spicetify-cli" "spotify" "vesktop-bin" "plymouth-theme-loader-2-git" "ttf-adobe-source-code-pro-fonts"
 )
 
+
+# === Step 1: Update pacman.conf ===
+cd ~
+sudo cp -f ~/i3/pacman.conf /etc/
+sudo cp -f ~/i3/nanorc /etc/
+
+# === Step 2: Install essential packages with pacman ===
+sudo pacman -Sy --noconfirm "${PACMAN_PACKAGES[@]}"
+
+# === Step 3: Install paru (AUR helper) and essential packeges ===
+git clone https://aur.archlinux.org/paru.git
+cd paru/
+makepkg -si --noconfirm
+cd ~
+sudo rm -rf paru/
+
+# === Step 4: Install essential packages with paru ===
+paru -Sy --noconfirm "${PARU_PACKAGES[@]}"
+
+# === Step 6: Clone and copy dotfiles ===
+sudo cp -r ~/i3/.config ~
 gsettings set org.gnome.desktop.interface gtk-theme 'Gruvbox-Green-Dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Gruvbox-Dark'
 gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Classic'
@@ -15,14 +36,8 @@ gsettings set org.gnome.desktop.interface font-name 'SourceCodePro 11'
 sudo chmod a+wr /opt/spotify
 sudo chmod a+wr /opt/spotify/Apps -R
 spicetify update
-sudo cp -f ~/Hypr/mkinitcpio.conf /etc/
-sudo mkinitcpio -p linux
-sudo cp -f ~/Hypr/grub /etc/default/
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-sudo plymout-default-theme -R loader_2
-sudo cp -f ~/Hypr/pltmouth.conf /etc/plymouth/
-sudo mkinitcpio -p linux
 
 # === Step 7: Final cleanup ===
 sudo pacman -Rns $(pacman -Qdtq) --noconfirm
 sudo pacman -Scc --noconfirm
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" & cp -f ~/i3/.bashrc ~ & sudo rm -rf ~/i3/ & reboot
