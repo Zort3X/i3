a680dcde62a5191718cd72f53f66a23aba6afa11
Z3R7 <marty.kamenow@gmail.com>
2025-01-07 22:07:58 +0200

    Fixed thunar font & added screenshot tool

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ PACMAN_PACKAGES=(
 )
 
 PARU_PACKAGES=(
-  "bibata-cursor-theme-bin" "google-chrome" "gruvbox-gtk-theme-git" "gruvbox-icon-theme-git" "spicetify-cli" "spotify" "vesktop-bin" "plymouth-theme-loader-2-git" "i3-rounded-border-patch-git" "i3-auto-tiling" "picom-simpleanims-next-git"
+  "bibata-cursor-theme-bin" "google-chrome" "gruvbox-gtk-theme-git" "gruvbox-icon-theme-git" "spicetify-cli" "spotify" "vesktop-bin" "plymouth-theme-loader-2-git" "i3-rounded-border-patch-git" "i3-auto-tiling" "picom-simpleanims-next-git" "gscreenshot"
 )
 
 
@@ -31,10 +31,6 @@ paru -Sy --noconfirm "${PARU_PACKAGES[@]}"
 sudo cp -r ~/i3/.config ~
 sudo cp -r ~/i3/.Xresources ~
 xrdb -merge ~/.Xresources
-gsettings set org.gnome.desktop.interface gtk-theme 'Gruvbox-Green-Dark'
-gsettings set org.gnome.desktop.interface icon-theme 'Gruvbox-Dark'
-gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Classic'
-gsettings set org.gnome.desktop.interface font-name 'SourceCodePro 11'
 sudo chmod +x ~/.config/lang_tog.sh
 sudo chmod a+wr /opt/spotify
 sudo chmod a+wr /opt/spotify/Apps -R
